05511d3dcd1cb50a0427d1f33372d940f10efe7c
Nya_Twisuki <100500860+Twisuki@users.noreply.github.com>
2024-12-05 14:09:36 +0800

    Add files via upload

--- Twisuki/marshoai-megakits/__init__.py
+++ Twisuki/marshoai-megakits/__init__.py
@@ -1,74 +1,37 @@
-import random
+
+from . import mk_Info
+from . import mk_Common
+from . import mk_MorseCode
+from . import mk_NyaCode
 
 # Twisuki
-async def twisuki () :
-    return "Twiuski(苏阳)是megakits插件作者, Github : 'https://github.com/Twisuki'"
+async def twisuki():
+    return str(mk_Info.twisuki())
 
 # MegaKits
-async def megakits () :
-    return "MegaKits插件是一个功能混杂的MarshoAI插件, 由Twisuki(Github : 'https://github.com/Twisuki')开发, 插件仓库 : 'https://github.com/Twisuki/marsho-toolsets/tree/main/Twisuki/marshoai-megakits'"
+async def megakits():
+    return str(mk_Info.megakits())
 
 # Random Turntable
-async def random_turntable (upper : int, lower : int = "0") :
-    return str(random.randint(lower, upper))
+async def random_turntable(upper: int, lower: int = "0"):
+    return str(mk_Common.random_turntable(upper, lower))
 
 # Number Calc
-async def number_calc (a : str, b : str, op : str) :
-    a, b = float(a), float(b)
-    match op :
-        case "+" : return str(a + b)
-        case "-" : return str(a - b)
-        case "*" : return str(a * b)
-        case "/" : return str(a / b)
-        case "**" : return str(a ** b)
-        case "%" : return str(a % b)
-        case _ : return "未知运算符"
-
-# MorseCode
-MorseCodeDICT = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
-    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
-    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
-    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-    'Y': '-.--', 'Z': '--..',
-    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
-    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
-    '.': '.-.-.-', ':': '---...', ',': '--..--', ';': '-.-.-.',
-    '?': '..--..', '=': '-...-', '\'': '.----.', '/': '-..-.',
-    '!': '-.-.--', '-': '-....-', '_': '..--.-', '\"': '.-..-.',
-    '(': '-.--.', ')': '-.--.-', '$': '...-..-', '&': '....',
-    '@': '.--.-.', ' ': ' '
-}
-MorseDecodeDICT = {value: key for key, value in MorseCodeDICT.items()}
+async def number_calc(a: str, b: str, op: str):
+    return str(mk_Common.number_calc(a, b, op))
 
 # MorseCode Encrypt
-async def morse_encrypt (msg : str) :
-    result = ""
-    msg = msg.upper()
-    for char in msg :
-        if char in MorseCodeDICT :
-            result += MorseCodeDICT[char]
-        else :
-            result += '..--..'
-        result += ' '
-
-    return result
+async def morse_encrypt(msg: str):
+    return str(mk_MorseCode.morse_encrypt(msg))
 
 # MorseCode Decrypt
-async def morse_decrypt (msg : str) :
-    result = ""
+async def morse_decrypt(msg: str):
+    return str(mk_MorseCode.morse_decrypt(msg))
 
-    msg_arr = msg.split()
-    for char in msg_arr :
-        if char in MorseDecodeDICT :
-            result += MorseDecodeDICT[char]
-        else :
-            result += '?'
+# NyaCode Encrypt
+async def nya_encode(msg: str):
+    return str(mk_NyaCode.nya_encode(msg))
 
-    return result
-
-# Latex
-async def latex (msg : str) :
-    return "https://latex.codecogs.com/png.image?" + msg
-
-# 
+# NyaCode Decrypt
+async def nya_decode(msg: str):
+    return str(mk_NyaCode.nya_decode(msg))
